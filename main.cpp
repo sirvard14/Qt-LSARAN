ae2856daf9cfe236f2d44d9049167a1bde218424
sirvard14 <sirvard2005@gmail.com>
2026-02-18 11:09:48 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
+#include "widget.h"
 #include <QApplication>
-#include <QWidget>
 #include <QLineEdit>
 #include <QSpinBox>
 #include <QProgressBar>
@@ -10,45 +10,54 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QWidget w;
+    Widget w;
 
-    QVBoxLayout v;
-    QHBoxLayout h1, h2, h3, h4, h5;
+    // Labels
+    QLabel *l1 = new QLabel("Name");
+    QLabel *l2 = new QLabel("Surname");
+    QLabel *l3 = new QLabel("Age");
+    QLabel *l4 = new QLabel("MOG");
+    QLabel *l5 = new QLabel("Progress");
 
+    // Inputs
+    QLineEdit *name = new QLineEdit;
+    QLineEdit *surname = new QLineEdit;
+    QSpinBox *age = new QSpinBox;
+    QLineEdit *mog = new QLineEdit;
+    QProgressBar *progress = new QProgressBar;
 
-    QLabel l1("Name");
-    QLabel l2("Surname");
-    QLabel l3("Age");
-    QLabel l4("MOG");
-    QLabel l5("Progress");
+    // Layouts
+    QVBoxLayout *mainLayout = new QVBoxLayout;
 
+    QHBoxLayout *h1 = new QHBoxLayout;
+    h1->addWidget(l1);
+    h1->addWidget(name);
 
-    QLineEdit Name;
-    QLineEdit Surname;
-    QSpinBox Age;
-    QLineEdit MOG;
-    QProgressBar progress;
+    QHBoxLayout *h2 = new QHBoxLayout;
+    h2->addWidget(l2);
+    h2->addWidget(surname);
 
-    h1.addWidget(&l1);
-    h1.addWidget(&Name);
-    h2.addWidget(&l2);
-    h2.addWidget(&Surname);
-    h3.addWidget(&l3);
-    h3.addWidget(&Age);
-    h4.addWidget(&l4);
-    h4.addWidget(&MOG);
-    h5.addWidget(&l5);
-    h5.addWidget(&progress);
+    QHBoxLayout *h3 = new QHBoxLayout;
+    h3->addWidget(l3);
+    h3->addWidget(age);
 
-    v.addLayout(&h1);
-    v.addLayout(&h2);
-    v.addLayout(&h3);
-    v.addLayout(&h4);
-    v.addLayout(&h5);
+    QHBoxLayout *h4 = new QHBoxLayout;
+    h4->addWidget(l4);
+    h4->addWidget(mog);
 
+    QHBoxLayout *h5 = new QHBoxLayout;
+    h5->addWidget(l5);
+    h5->addWidget(progress);
 
-    w.setLayout(&v);
+    mainLayout->addLayout(h1);
+    mainLayout->addLayout(h2);
+    mainLayout->addLayout(h3);
+    mainLayout->addLayout(h4);
+    mainLayout->addLayout(h5);
+
+    w.setLayout(mainLayout);
     w.show();
 
     return a.exec();
 }
+
