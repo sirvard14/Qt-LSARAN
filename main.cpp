6b2b8e255b96eb79fbe6e2a2b9d8ec1a47c360a6
sirvard14 <sirvard2005@gmail.com>
2026-02-18 11:33:22 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,63 +1,66 @@
-#include "widget.h"
 #include <QApplication>
+#include <QWidget>
 #include <QLineEdit>
 #include <QSpinBox>
 #include <QProgressBar>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QDate>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+    QWidget w;
 
-    // Labels
+    QVBoxLayout *v = new QVBoxLayout;
+    QHBoxLayout *h1 = new QHBoxLayout;
+    QHBoxLayout *h2 = new QHBoxLayout;
+    QHBoxLayout *h3 = new QHBoxLayout;
+    QHBoxLayout *h4 = new QHBoxLayout;
+    QHBoxLayout *h5 = new QHBoxLayout;
+
     QLabel *l1 = new QLabel("Name");
     QLabel *l2 = new QLabel("Surname");
     QLabel *l3 = new QLabel("Age");
     QLabel *l4 = new QLabel("MOG");
     QLabel *l5 = new QLabel("Progress");
+    QLabel *dateLabel = new QLabel(QDate::currentDate().toString());
 
-    // Inputs
-    QLineEdit *name = new QLineEdit;
-    QLineEdit *surname = new QLineEdit;
-    QSpinBox *age = new QSpinBox;
-    QLineEdit *mog = new QLineEdit;
+    QLineEdit *Name = new QLineEdit;
+    QLineEdit *Surname = new QLineEdit;
+    QSpinBox *Age = new QSpinBox;
+    QLineEdit *MOG = new QLineEdit;
     QProgressBar *progress = new QProgressBar;
 
-    // Layouts
-    QVBoxLayout *mainLayout = new QVBoxLayout;
+    progress->setRange(0, 100);
 
-    QHBoxLayout *h1 = new QHBoxLayout;
     h1->addWidget(l1);
-    h1->addWidget(name);
+    h1->addWidget(Name);
 
-    QHBoxLayout *h2 = new QHBoxLayout;
     h2->addWidget(l2);
-    h2->addWidget(surname);
+    h2->addWidget(Surname);
 
-    QHBoxLayout *h3 = new QHBoxLayout;
     h3->addWidget(l3);
-    h3->addWidget(age);
+    h3->addWidget(Age);
 
-    QHBoxLayout *h4 = new QHBoxLayout;
     h4->addWidget(l4);
-    h4->addWidget(mog);
+    h4->addWidget(MOG);
 
-    QHBoxLayout *h5 = new QHBoxLayout;
     h5->addWidget(l5);
     h5->addWidget(progress);
+    h5->addWidget(dateLabel);
 
-    mainLayout->addLayout(h1);
-    mainLayout->addLayout(h2);
-    mainLayout->addLayout(h3);
-    mainLayout->addLayout(h4);
-    mainLayout->addLayout(h5);
+    v->addLayout(h1);
+    v->addLayout(h2);
+    v->addLayout(h3);
+    v->addLayout(h4);
+    v->addLayout(h5);
 
-    w.setLayout(mainLayout);
+    w.setLayout(v);
+    w.setStyleSheet("color: white; background-color: pink;");
+    w.resize(400,400);
     w.show();
 
     return a.exec();
 }
-
